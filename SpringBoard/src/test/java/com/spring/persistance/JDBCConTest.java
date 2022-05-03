de717de43ab6bf54533dcf3c318737f8b1d1e4ae
jyr940717 <whddydfh@naver.com>
2022-05-03 15:17:42 +0900

    hikaricp 와 mybatis 연동

---
+++ SpringBoard/src/test/java/com/spring/persistance/JDBCConTest.java
@@ -0,0 +1,33 @@
+package com.spring.persistance;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+public class JDBCConTest {
+	 static { 
+	        try { 
+	            Class.forName("oracle.jdbc.driver.OracleDriver"); 
+	        } catch(Exception e) { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+	    
+	    @Test 
+	    public void testConnection() { 
+	        try(Connection con = DriverManager.getConnection( 
+	                "jdbc:oracle:thin:@localhost:1521/xe", 
+	                "springbd", 
+	                "1234")){ 
+	            System.out.println(con); 
+	        } catch (Exception e) { 
+	            fail(e.getMessage()); 
+	        } 
+	    
+	    }   
+
+}
